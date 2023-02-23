dcd885f26343223377898c07dbffb943cee56f89
angie2004 <laurasilvamar3@gmail.com>
2023-02-23 11:27:54 -0500

    v1

---
+++ Angie Parada/formulario/script.js
@@ -0,0 +1,15 @@
+function capturar() {
+
+    var nombre = document.getElementById('nombre').value
+    var direccion = document.getElementById('direccion').value
+    var documento = document.getElementById('documento').value
+    var correo = document.getElementById('correo').value
+    var telefono = document.getElementById('telefono').value
+
+    console.log(nombre)
+    console.log(direccion)
+    console.log(documento)
+    console.log(correo)
+    console.log(telefono)
+
+}
