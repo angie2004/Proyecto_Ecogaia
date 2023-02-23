1d7f8e8097927db3469349383118173959204199
Valeria <dannasuarez786@gmail.com>
2023-02-23 11:03:09 -0500

    v2

---
+++ Valeria/formulario.js
@@ -0,0 +1,9 @@
+function mensaje(){
+     var mensaje = confirm("¿Esta seguro que quiere agregar este producto?");
+     if (mensaje) {
+     alert("¡Gracias por confirmar!");
+     }
+     else {
+     alert("¡Haz denegado el mensaje!");
+     }
+}
